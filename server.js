99792b02bd7476fd50ca2c19f7df3474a2a2fa2a
Porcescu Ion <prc_ion@outlook.com>
2019-10-31 16:52:45 +0200

    add database in project

--- server.js
+++ server.js
@@ -1,66 +1,66 @@
 //connect framework express
 let express = require('express')
+let mongodb = require('mongodb')
 let app = express()
+
+let db
+
+let connectionString = 'mongodb+srv://toDoAppUser:Makar!23Barak@RED@cluster0-fmjmq.mongodb.net/TodoApp?retryWrites=true&w=majority'
+mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true  },function(err, client){
+  db = client.db()
+  app.listen(3000)
+})
 
 app.use(express.urlencoded({extended: false}))
 
 //the first page
 app.get('/', function(require, response){
-  response.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Simple To-Do App</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-      <div class="container">
-        <h1 class="display-4 text-center py-1">To-Do App</h1>
+  db.collection('items').find().toArray(function(err, items){
+    response.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple To-Do App</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+      </head>
+      <body>
+        <div class="container">
+          <h1 class="display-4 text-center py-1">To-Do App</h1>
 
-        <div class="jumbotron p-3 shadow-sm">
-          <form action="/create-item" method="POST">
-            <div class="d-flex align-items-center">
-              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-              <button class="btn btn-primary">Add New Item</button>
-            </div>
-          </form>
+          <div class="jumbotron p-3 shadow-sm">
+            <form action="/create-item" method="POST">
+              <div class="d-flex align-items-center">
+                <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                <button class="btn btn-primary">Add New Item</button>
+              </div>
+            </form>
+          </div>
+
+          <ul class="list-group pb-5">
+            ${items.map(function(elem){
+              return`
+              <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                <span class="item-text">${elem.text}</span>
+                <div>
+                  <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                  <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                </div>
+              </li>`
+            }).join('')}
+          </ul>
+
         </div>
 
-        <ul class="list-group pb-5">
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #1</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #2</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #3</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-        </ul>
-
-      </div>
-
-    </body>
-    </html>
-  `)
+      </body>
+      </html>
+    `)
+  })
 })
 
 app.post('/create-item', function(request, response){
-  console.log(request.body.item)
-  response.send("Thanks for submitting the form.")
+  db.collection('items').insertOne({text: request.body.item}, function(){
+    response.redirect('/')
+  })
 })
-app.listen(3000)
